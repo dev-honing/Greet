91422363007bd57aa272ff07412af8b1841d2542
Ho Nyeong <dev.honing@gmail.com>
2024-05-22 02:46:07 +0900

    Feat: class와 멤버 변수, 멤버 함수를 사용하여 인사말 출력 프로그램 완성

--- Greet.cpp
+++ Greet.cpp
@@ -6,9 +6,52 @@
 */
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Greet {
+private:
+    string m_name;
+    string m_time;
+
+public:
+    void greet() const {
+        cout << m_name << "님 안녕하세요, 좋은 " << m_time << "입니다!" << endl;
+    }
+
+    string getName() const {
+        return m_name;
+    }
+
+    string getTime() const {
+        return m_time;
+    }
+
+    void setName(const string name) {
+        m_name = name;
+    }
+
+    void setTime(const string time) {
+        m_time = time;
+    }
+};
 
 int main() {
-	printf("Hello, C++!");
+    Greet greetObj;
+    string name, time;
 
-	return 0;
+    cout << "이름을 입력하세요: ";
+    getline(cin, name);
+
+    // 시간대
+    cout << "시간대를 입력하세요 (예: 아침, 오후, 저녁): ";
+    getline(cin, time);
+
+    greetObj.setName(name);
+    greetObj.setTime(time);
+
+    greetObj.greet();
+
+    return 0;
 }
